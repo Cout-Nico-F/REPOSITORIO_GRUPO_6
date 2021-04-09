88361d14a53e74d7a860a68331cbfc2df36e46b2
Alonso <61801479+AlonsoHS21@users.noreply.github.com>
2021-04-09 19:46:37 -0300

    3 - Creando los contructores

--- TP2/src/ejercicio1/Profesor.java
+++ TP2/src/ejercicio1/Profesor.java
@@ -4,6 +4,18 @@ public class Profesor extends Empleado {
 	String cargo;
 	int antiguedadDocente;
 	
+	//Contructors
+	public Profesor() {
+		super();
+		cargo = "sin cargo";
+		antiguedadDocente = 0;
+	}
+	public Profesor(String nombre,byte edad,String cargo,int antiguedadDocente) {
+		super(nombre,edad);
+		this.cargo = cargo;
+		this.antiguedadDocente = antiguedadDocente;
+	}
+	
 	//Encapsulamiento (Getters and Setters)
 	public String getCargo() {
 		return cargo;
