d202331bc0e0914797e72c5006113a33a79629a7
Cout-Nico-F <n.ferreiraa@live.com.ar>
2021-04-19 15:46:08 -0300

    Clase Principal (Vacia + comentarios)

---
+++ TP3/src/ejercicio1/Principal.java
@@ -0,0 +1,8 @@
+package ejercicio1;
+
+public class Principal {
+
+	//Lee el archivo personas.txt, por cada registro llama al metodo verificarDNI y lo guarda en una lista solo si es valido
+	//Escribe a resultado.txt.
+	
+}
