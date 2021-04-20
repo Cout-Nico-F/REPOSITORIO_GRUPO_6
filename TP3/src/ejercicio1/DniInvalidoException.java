fac148a563eba9ffced22e176ff745e318fc8df9
Cout-Nico-F <n.ferreiraa@live.com.ar>
2021-04-20 18:03:03 -0300

    Excepcion personalizada y clase Principal

---
+++ TP3/src/ejercicio1/DniInvalidoException.java
@@ -0,0 +1,11 @@
+package ejercicio1;
+
+import java.io.IOException;
+
+public class DniInvalidoException extends IOException {
+	
+	public DniInvalidoException() {
+		//codigo que ejecutar si se produce la excepcion
+	}
+	
+}
--- TP3/src/ejercicio1/Principal.java
+++ TP3/src/ejercicio1/Principal.java
@@ -4,5 +4,20 @@ public class Principal {
 
 	//Lee el archivo personas.txt, por cada registro llama al metodo verificarDNI y lo guarda en una lista solo si es valido
 	//Escribe a resultado.txt.
+	public static void main(String[] args) {
+		
+		
+	}
+	
+	
+	void VerificarDniInvalido () throws DniInvalidoException {
+		//verifica que contenga solo numeros
+		//convierte un int en un string y compara cada caracter
+		//puede usar el codigo ascii para saber si es numero
+		
+		if (/*tiene caracteres que no son numeros*/) {
+			throw new DniInvalidoException();
+		}
+	}
 	
 }
