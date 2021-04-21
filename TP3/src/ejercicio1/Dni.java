1b459dfb3c5cbd5fe42c51cc43458265b06d34df
Cristian Paolini <cristianpaolini3@gmail.com>
2021-04-21 16:08:17 -0300

    Creación de clase Dni y método validarDni desarrollado.

---
+++ TP3/src/ejercicio1/Dni.java
@@ -0,0 +1,37 @@
+package ejercicio1;
+
+public class Dni {
+	
+	public Dni() {
+		
+	}
+	
+	public static boolean validarDni(String dni) throws DniInvalidoException {
+		
+		Boolean auxLongitud = false;
+
+
+		if (dni.length() == 8) {
+			auxLongitud = true;
+			
+		} else {
+			throw new DniInvalidoException(DniInvalidoException.LONGITUD_NO_VALIDA);
+		}
+		
+		int DniException = 0;
+		
+		try {
+			DniException = Integer.parseInt(dni);
+
+		} catch (NumberFormatException e) {
+			throw new DniInvalidoException(DniInvalidoException.FORMATO_NO_VALIDO);
+		}
+		
+		
+		if (auxLongitud && DniException != 0) {
+			return true;
+		} return false;
+	}
+	
+
+}
--- TP3/src/ejercicio1/DniInvalidoException.java
+++ TP3/src/ejercicio1/DniInvalidoException.java
@@ -4,8 +4,24 @@ import java.io.IOException;
 
 public class DniInvalidoException extends IOException {
 	
+	public static final String LONGITUD_NO_VALIDA = "La longitud debe ser de 8 caracteres.";
+	
+	public static final String FORMATO_NO_VALIDO = "El Dni debe estar conformado por 8 caracteres entre el 0 al 9.";
+	
 	public DniInvalidoException() {
 		//codigo que ejecutar si se produce la excepcion
 	}
+	
+	public DniInvalidoException(String exception) {
+		
+	}
+
+	@Override
+	public String getMessage() {
+
+		return "El DNI ingresado no es válido.";
+	}
+	
+	
 	
 }
