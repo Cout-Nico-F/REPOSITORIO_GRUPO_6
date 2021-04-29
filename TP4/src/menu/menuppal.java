555fa1bee2a384133b2ae827315970ecdc5518c1
mledesma3 <ledesma.mn3@gmail.com>
2021-04-29 20:32:06 -0300

    JFrames principal y JFrames de ejercicio 1.

---
+++ TP4/src/menu/menuppal.java
@@ -0,0 +1,67 @@
+package menu;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import java.awt.Font;
+
+public class menuppal extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					menuppal frame = new menuppal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public menuppal() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnEjercicio1 = new JButton("Ejercicio 1");
+		btnEjercicio1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnEjercicio1.setBounds(154, 63, 97, 25);
+		contentPane.add(btnEjercicio1);
+		
+		JButton btnEjercicio2 = new JButton("Ejercicio 2");
+		btnEjercicio2.setBounds(154, 119, 97, 25);
+		contentPane.add(btnEjercicio2);
+		
+		JButton btnEjercicio3 = new JButton("Ejercicio 3");
+		btnEjercicio3.setBounds(154, 170, 97, 25);
+		contentPane.add(btnEjercicio3);
+		
+		JLabel lblGrupoN = new JLabel("GRUPO N\u00B0 6");
+		lblGrupoN.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblGrupoN.setBounds(38, 34, 135, 16);
+		contentPane.add(lblGrupoN);
+	}
+}
