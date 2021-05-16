6b84dbd11674ff205e8f237f64358d1ca50c7099
Cout-Nico-F <n.ferreiraa@live.com.ar>
2021-05-16 14:06:38 -0300

    Movi todos los controles un poco hacia arriba porque el aceptar se veia cortado

--- TP5/src/aplicacion/PanelAgregarPelicula.java
+++ TP5/src/aplicacion/PanelAgregarPelicula.java
@@ -21,28 +21,28 @@ public class PanelAgregarPelicula extends JPanel {
 		
 		JLabel lblid = new JLabel("ID");
 		lblid.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblid.setBounds(73, 73, 46, 14);
+		lblid.setBounds(71, 40, 46, 14);
 		add(lblid);
 		
 		JLabel lblNombre = new JLabel("Nombre");
 		lblNombre.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblNombre.setBounds(73, 114, 63, 14);
+		lblNombre.setBounds(71, 81, 63, 14);
 		add(lblNombre);
 		
 		JLabel lblGenero = new JLabel("Genero");
 		lblGenero.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblGenero.setBounds(73, 161, 46, 14);
+		lblGenero.setBounds(71, 128, 46, 14);
 		add(lblGenero);
 		
 		tfNombre = new JTextField();
 		tfNombre.setFont(new Font("Tahoma", Font.BOLD, 13));
-		tfNombre.setBounds(159, 111, 118, 20);
+		tfNombre.setBounds(157, 78, 118, 20);
 		add(tfNombre);
 		tfNombre.setColumns(10);
 		
 		JButton btnAceptar = new JButton("Aceptar");
 		btnAceptar.setFont(new Font("Tahoma", Font.BOLD, 13));
-		btnAceptar.setBounds(73, 212, 89, 23);
+		btnAceptar.setBounds(71, 179, 89, 23);
 		add(btnAceptar);
 		
 		cbGenero = new JComboBox<String>();
@@ -53,13 +53,13 @@ public class PanelAgregarPelicula extends JPanel {
 		cbGenero.addItem("Romantica");
 		cbGenero.setToolTipText("Seleccione un genero");
 		cbGenero.setFont(new Font("Tahoma", Font.BOLD, 13));
-		cbGenero.setBounds(159, 158, 118, 20);
+		cbGenero.setBounds(157, 125, 118, 20);
 		add(cbGenero);
 		
 		JLabel lblID = new JLabel("");
 		lblID.setText(String.valueOf(Pelicula.getCount()));
 		lblID.setFont(new Font("Tahoma", Font.BOLD, 13));
-		lblID.setBounds(161, 74, 56, 14);
+		lblID.setBounds(159, 41, 56, 14);
 		add(lblID);
 
 	}
