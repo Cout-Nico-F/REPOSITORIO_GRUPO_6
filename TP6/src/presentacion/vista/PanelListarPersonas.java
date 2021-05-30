b4a8f7e66e375a98fd3d8497691aba269ec1b6ec
Cout-Nico-F <n.ferreiraa@live.com.ar>
2021-05-30 19:34:34 -0300

    Avances grupales en zoom

--- TP6/src/presentacion/vista/PanelListarPersonas.java
+++ TP6/src/presentacion/vista/PanelListarPersonas.java
@@ -1,9 +1,17 @@
 package presentacion.vista;
 
+import java.util.List;
+
+import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+
+import entidad.Persona;
+import negocio.IPersonaNegocio;
+import negocioImpl.PersonaNegocioImpl;
 
 public class PanelListarPersonas extends JPanel {
 	
@@ -13,7 +21,13 @@ public class PanelListarPersonas extends JPanel {
 	
 	public PanelListarPersonas() {
 		setLayout(null);
+		
+		IPersonaNegocio pneg = new PersonaNegocioImpl();
+		List<Persona> listaPersonas = pneg.readAll();
+		
 		modelPersonas = new DefaultTableModel(null,nombreColumnas);
+		//modelPersonas.
+		
 		tablaPersonas = new JTable(modelPersonas);
 		tablaPersonas.setBounds(10, 11, 380, 253);
 		
@@ -24,11 +38,17 @@ public class PanelListarPersonas extends JPanel {
 		tablaPersonas.getColumnModel().getColumn(2).setPreferredWidth(100);
 		tablaPersonas.getColumnModel().getColumn(2).setResizable(false);
 		
+		//cargar la lista desde base de datos (comunicarse con negocio)
+		//ComunicarseConNegocio();
+		
 		JScrollPane spPersonas = new JScrollPane();
+		
 		spPersonas.setBounds(15, 11, 380, 250);
 		add(spPersonas);
 		
 		spPersonas.setViewportView(tablaPersonas);
 
 	}
+
 }
+
