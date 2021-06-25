6533043a50d0d5d76f77d5f7cb6f45ce8b87f10e
Alonso <61801479+AlonsoHS21@users.noreply.github.com>
2021-06-25 17:05:51 -0300

    update insert creando un usuario admin y un cliente con otro usuario

--- Final/ScriptingBancos/inserts_bdlabo.sql
+++ Final/ScriptingBancos/inserts_bdlabo.sql
@@ -6,11 +6,16 @@ insert into nacionalidades (IdNacionalidad,Nombre) values (1,"Agentina");
 insert into provincias (IdProvincia,Nombre) values (1,"Buenos Aires");
 insert into localidades (IdLocalidad,IdProvincia,Nombre) values (1,1,"Escobar");
 
-# -- Para crear un cliente primero tiene que tener un cuenta -- Agrego un usuario para probar el login --
+# -- Para crear un cliente primero tiene que tener un cuenta 
 insert into tiposusuarios (IdTipoUsuario,Descripcion) values (1,"Cliente");
 insert into tiposusuarios (IdTipoUsuario,Descripcion) values (2,"Admin");
+
+-- Agrego un usuario para probar el login --
 insert into usuarios (IdUsuario,IdTipoUsuario,NombreUsuario,Contrasenia) values (1,2,"AlonsoHS20","12345"); #tendriamos que encriptar la contrasenia
 
-# -- Agrego un cliente --
+# -- Agrego un cliente y a ese cliente le creo un usuario --
+insert into usuarios (IdUsuario,IdTipoUsuario,NombreUsuario,Contrasenia) values (2,1,"Nose","123"); #tendriamos que encriptar la contrasenia -- tiene que tener un usuario antes para que pueda ser cliente?
 insert into clientes (Dni,IdUsuario,IdNacionalidad,IdLocalidad,Cuil,Nombre,Apellido,Sexo,FechaNacimiento,Direccion,CorreoElectronico) 
-values (14203944,1,1,1,111111111111,"Alonso","Huarcaya","Masculino","2021/06/25","Av.Siempre viva 123","prueba@gmail.com");
+values (14203944,2,1,1,111111111111,"Nose","Valdez","Masculino","2021/06/25","Av.Siempre viva 123","prueba@gmail.com");
+
+
