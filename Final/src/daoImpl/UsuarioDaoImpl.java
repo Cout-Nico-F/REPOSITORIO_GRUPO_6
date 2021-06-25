a4af28c58133b66f2732ff4c8d3e4b6f64f0d30e
Alonso <61801479+AlonsoHS21@users.noreply.github.com>
2021-06-25 17:50:56 -0300

    Creando el metodo para buscar un usuario

--- Final/src/daoImpl/UsuarioDaoImpl.java
+++ Final/src/daoImpl/UsuarioDaoImpl.java
@@ -2,15 +2,38 @@ package daoImpl;
 
 import java.sql.SQLException;
 import entidad.Usuario;
+import daoImpl.Conexion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class UsuarioDaoImpl {
 	
-	private static final String getUsuario = "Select * From usuarios Where nombreUsuario='u.getnombreUsuario' and contrasenia='u.getContrasenia'";
+	private static final String searchUser = "Select * From usuarios Where nombreUsuario=? and contrasenia=?";
 	
-	public boolean getUsuario(Usuario u) throws SQLException
+	public boolean buscarUsuario(Usuario u) throws SQLException
 	{
 		//Codigo para buscar el usuario y que devuelve un bool en caso de existir
 		//Cuando la bd este terminada haria un procedimiento almacenado
-		return true;
+		PreparedStatement statement;
+		Connection conexion = Conexion.getConexion().getSQLConexion();
+		ResultSet rs = null;
+		boolean busquedaExitosa = false;
+		try {
+			statement = conexion.prepareStatement(searchUser);
+			statement.setString(1,u.getNombreUsuario());
+			statement.setString(2,u.getContrasenia());
+			rs = statement.executeQuery();
+			if(rs.absolute(1)) //Mueve el cursor a la primera fila devuelve 1 - si trajo 1 entonces es que los datos coinciden
+			{
+				return true;
+			}
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		
+		return busquedaExitosa;
 	}
 }
