06b06007b7933ad71a5f291d91e1e906d10fe681
Alonso <61801479+AlonsoHS21@users.noreply.github.com>
2021-06-25 15:06:35 -0300

    script de la creacion de la base de datos terminado
    La seccion Modify esta toda comentada porque hay errores con el tema de FKs

--- Final/ScriptingBancos/bdlabo.sql
+++ Final/ScriptingBancos/bdlabo.sql
@@ -5,100 +5,98 @@ create database bdlabo;
 use bdlabo;
 
 create table if not exists clientes (
-	IdUsuario int unsigned not null, #Es autoincrementable
+	Dni int not null, #unique el valor ingresado no se puede repetir en registros posteriores
+	IdUsuario int unsigned not null, #Es autoincrementable le agregue unique porque sino no dejaba que tipoUsuarios lo tenga como FK
     IdNacionalidad int not null,
     IdLocalidad int not null,
-	Dni int unique not null, #unique el valor ingresado no se puede repetir en registros posteriores
     Cuil bigint unique not null, 
     Nombre varchar(45) not null,
     Apellido varchar(45) not null,
     Sexo varchar(20) not null, #Serian masculino - femenino - otro enviamos directamente la cadena las opciones van a ser seleccionables no es un input de texto
     FechaNacimiento datetime not null,
     Direccion varchar(45) not null,
-    CorreoElectronico varchar(45) not null,
-	primary Key (IdUsuario,Dni)
+    CorreoElectronico varchar(45) not null
 );
 create table if not exists usuarios (
 	IdUsuario int unsigned not null,
-    IdTipoUsuario tinyint(1) unsigned unique not null, #tinyint(1) ya que en MySQL no hay un tipo de datos booleano 
+    IdTipoUsuario tinyint unsigned not null, #tinyint(1) ya que en MySQL no hay un tipo de datos booleano 
     #Error 1822: Para la relacion de foreign key, la columna de la tabla principal en la que se esta creando la relacion debe ser unique o primary y deben tener el mismo datatype y size.
     NombreUsuario varchar(45) not null,
     Contrasenia varchar(45) not null
 );
 create table if not exists tiposUsuarios (
-	IdTipoUsuario tinyint(1) unsigned unique not null,
+	IdTipoUsuario tinyint unsigned not null,
     Descripcion varchar(45) not null
 );
 create table if not exists telefonos(
-	Dni int unique not null,
-    Telefono int unique not null,
+	Dni int not null,
+    Telefono int not null,
     Primary Key(Dni,Telefono)
 );
-create table if not exists nacionalidad (
+create table if not exists nacionalidades (
 	IdNacionalidad int not null,
     Nombre varchar(45) not null
 );
-create table if not exists localidad (
+create table if not exists localidades (
 	IdLocalidad int not null,
     IdProvincia int not null,
     Nombre varchar(45) not null
 );
-create table if not exists provincia (
+create table if not exists provincias (
 	IdProvincia int not null,
     Nombre varchar(45) not null
 );
-create table if not exists cuenta (
-	NumeroCuenta bigint unique not null, #Hay que validar que sean numeros 
-    Dni int unique not null,
-    IdTipoCuenta tinyint(1) unsigned unique not null, #Solo hay 2 tipos de cuentas por eso tinyint(1)
+create table if not exists cuentas (
+	NumeroCuenta bigint not null, #Hay que validar que sean numeros 
+    Dni int not null,
+    IdTipoCuenta tinyint unsigned not null, #Solo hay 2 tipos de cuentas por eso tinyint(1) esta obsoleto ya lo saque
     Saldo decimal not null,
     Cbu int unique not null,
     FechaCreacion datetime not null
 );
-create table if not exists tipoCuenta (
-	IdTipoCuenta tinyint(1) unsigned unique not null,
+create table if not exists tiposDeCuenta (
+	IdTipoCuenta tinyint unsigned not null,
     Descripcion varchar(45) not null
 );
 create table if not exists prestamos (
-	IdPrestamos int unsigned unique, #primero hacemos que sea pk y despues lo modificamos para que sea autoincrementable
-    NumeroCuenta bigint unique not null, #Los numeros de  cuenta no se repiten? por las dudas unique
-	Dni int unique not null,
+	IdPrestamos int unsigned not null, #primero hacemos que sea pk y despues lo modificamos para que sea autoincrementable
+    NumeroCuenta bigint not null, #Los numeros de  cuenta no se repiten? por las dudas unique
+	Dni int not null,
     Fecha datetime not null,
     ImporteSolicitado decimal not null,
     ImporteAPagar decimal not null,
     PlazoPagoMeses decimal not null,
     MontoMensual decimal not null,
     Cuotas tinyint unsigned not null,
-    Estado tinyint(1) not null #En MySQL, cero se considera falso y el valor distinto de cero se considera verdadero. Para usar literales booleanos,
+    Estado tinyint not null #En MySQL, cero se considera falso y el valor distinto de cero se considera verdadero. Para usar literales booleanos,
 );
 create table if not exists cuotas (
-	IdPrestamos int unsigned unique,
-    NumeroCuenta bigint unique not null,
+	IdPrestamos int unsigned not null,
+    NumeroCuota tinyint not null,
     Importe decimal not null, # que tipo de importe es?
     FechaVencimiento datetime not null,
     FechaPago datetime not null,
-    Primary Key (IdPrestamos,NumeroCuenta)
+    Primary Key (IdPrestamos,NumeroCuota)
 );
 create table if not exists movimientos (
-	IdMovimientos int unsigned unique, #primero hacemos que sea pk y despues lo modificamos para que sea autoincrementable
-	IdTipoMovimiento tinyint unsigned unique not null,
-	Dni int unique not null,
-    CuentaOrigen bigint unique not null,
-    CuentaDestino bigint unique not null,	
+	IdMovimientos int unsigned not null, #primero hacemos que sea pk y despues lo modificamos para que sea autoincrementable
+	IdTipoMovimiento tinyint unsigned not null,
+	Dni int not null,
+    CuentaOrigen bigint not null,
+    CuentaDestino bigint not null,	
     Fecha datetime not null,
     Detalles text not null,
     Importe decimal not null
 );
 create table if not exists tiposMovimientos (
-	IdTipoMovimiento tinyint unsigned unique not null,
+	IdTipoMovimiento tinyint unsigned not null,
     Descripcion varchar(45) not null
 );
 
-#Primary Key
+# -------------------------------------------------- PRIMARY KEY --------------------------------------------------
 
 # -- Clientes --
 alter table clientes add primary key (Dni);
-#alter table clientes modify IdUsuario int unsigned auto_increment; #para que pueda ser autoincrementable primero tiene que ser pk
 
 # -- Usuarios --
 alter table usuarios add primary key (IdUsuario);
@@ -110,90 +108,110 @@ alter table tiposUsuarios add primary key (IdTipoUsuario);
 # Los pks estan declarados en la tabla
 
 # -- Nacionalidad --
-alter table nacionalidad add primary key (IdNacionalidad);
-alter table nacionalidad modify IdNacionalidad int unsigned auto_increment;
+alter table nacionalidades add primary key (IdNacionalidad);
 
 # -- Localidad --
-alter table localidad add primary key (IdLocalidad);
-alter table localidad modify IdLocalidad int unsigned auto_increment;
+alter table localidades add primary key (IdLocalidad);
 
 # -- Provincia --
-alter table provincia add primary key (IdProvincia);
-alter table provincia modify IdProvincia int unsigned auto_increment;
+alter table provincias add primary key (IdProvincia);
 
 # -- Cuenta --
-alter table cuenta add primary key (NumeroCuenta);
+alter table cuentas add primary key (NumeroCuenta);
 
 # -- Tipo Cuentas --
-alter table tipoCuenta add primary key (IdTipoCuenta);
+alter table tiposDeCuenta add primary key (IdTipoCuenta);
 
 # -- Prestamos --
 alter table prestamos add primary key (IdPrestamos);
-alter table prestamos modify IdPrestamos int unsigned auto_increment; #De forma predeterminada empieza en 1
-#Dato el atributo auto_increment no es compatible con el datatype tinyint 
 
 # -- Cuotas --
 # Los pks estan declarados en la tabla
 
 # -- Movimientos --
 alter table movimientos add primary key (IdMovimientos);
-alter table movimientos modify IdMovimientos int unsigned unique auto_increment;
 
 # -- Tipos Movimientos --
 alter table tiposMovimientos add primary key (IdTipoMovimiento);
 
+#  --------------------------------------------------  MODIFY --------------------------------------------------
 
-# FOREIGN KEY
+# -- Clientes --
+#alter table clientes modify IdLocalidad int unsigned auto_increment;
+#alter table clientes modify IdNacionalidad int unsigned auto_increment;
+
+# -- Usuarios -- 
+#alter table usuarios modify IdUsuario int unsigned auto_increment; #para que pueda ser autoincrementable primero tiene que ser pk o unique
+
+# -- Nacionalidad --
+#alter table nacionalidades modify IdNacionalidad int unsigned auto_increment not null;
+
+# -- Localidad --
+#alter table localidades modify IdLocalidad int unsigned auto_increment not null;
+#alter table localidades modify IdProvincia int unsigned auto_increment not null;
+
+# -- Provincia --
+#alter table provincias modify IdProvincia int unsigned auto_increment not null;
+
+# -- Prestamos --
+#alter table prestamos modify IdPrestamos int unsigned auto_increment; #De forma predeterminada empieza en 1
+#Dato el atributo auto_increment no es compatible con el datatype tinyint 
+
+# -- Cuotas -- 
+#alter table cuotas modify IdPrestamos int unsigned auto_increment;
+
+# -- Movimientos -- 
+#alter table movimientos modify IdMovimientos int unsigned auto_increment;
+
+
+# -------------------------------------------------- FOREIGN KEY --------------------------------------------------
 
 # --Clientes --
-
+alter table clientes add foreign key (IdUsuario) references usuarios (IdUsuario); #Error 1822 IdUsuario tenia que ser unique o pk
+alter table clientes add foreign key (IdLocalidad) references localidades (IdLocalidad);
+alter table clientes add foreign key (IdNacionalidad) references nacionalidades (IdNacionalidad);
 
 # -- Usuarios --
-alter table usuarios add foreign key (IdUsuario) references clientes (IdUsuario);
- 
+alter table usuarios add foreign key (IdTipoUsuario) references tiposUsuarios (IdTipoUsuario); #Por defecto cuando se crea una cuenta se setea en 1
+
 # -- Tipos usuarios --
-alter table tiposUsuarios add foreign key (IdTipoUsuario) references usuarios (IdTipoUsuario); #Por defecto cuando se crea una cuenta se setea en 1
+
+
+# -- Telefono --
+alter table telefonos add foreign key (Dni) references clientes (Dni);
+ 
+# -- Nacionalidad --
+
+
+# -- Localidad --
+alter table localidades add foreign key (IdProvincia) references provincias (IdProvincia);
+
+# -- Provincia --
 
 # -- Cuenta --
-alter table cuenta add foreign key (Dni) references clientes (Dni);
-#alter table cuenta add foreign key (IdTipoCuenta) references tipoCuenta (IdTipoCuenta);
+alter table cuentas add foreign key (Dni) references clientes (Dni);
+alter table cuentas add foreign key (IdTipoCuenta) references tiposDeCuenta (IdTipoCuenta);
 
 # -- Tipo Cuentas --
-alter table tipoCuenta add foreign key (IdTipoCuenta) references cuenta (IdTipoCuenta);
+alter table tiposDeCuenta add foreign key (IdTipoCuenta) references cuentas (IdTipoCuenta);
 
 # -- Prestamos --
 alter table prestamos add foreign key (Dni) references clientes (Dni);
-alter table prestamos add foreign key (NumeroCuenta) references cuenta (NumeroCuenta);
+alter table prestamos add foreign key (NumeroCuenta) references cuentas (NumeroCuenta);
+
+# -- Cuotas --
+alter table cuotas add foreign key (IdPrestamos) references prestamos (IdPrestamos);
 
 # -- Movimientos --
 alter table movimientos add foreign key (Dni) references clientes (Dni);
-#alter table movimientos add foreign key (IdTipoMovimiento) references tiposMovimientos (IdTipoMovimiento); esto va aca?
+alter table movimientos add foreign key (IdTipoMovimiento) references tiposMovimientos (IdTipoMovimiento);
 
 # -- Tipos Movimientos --
-alter table tiposMovimientos add foreign key (IdTipoMovimiento) references movimientos (IdTipoMovimiento);
-
-# Estos datos van a ser harcodeados por nosotros supongo
-
-# -- Nacionalidad --
-alter table nacionalidad add foreign key (IdNacionalidad) references clientes (IdNacionalidad);
-
-# -- Localidad --
-alter table localidad add foreign key (IdLocalidad) references clientes (IdLocalidad);
-
-# -- Provincia --
-alter table provincia add foreign key (IdProvincia) references localidad (IdProvincia);
 
 
-# Agregando un cliente
-Insert into clientes (idNacionalidad,IdLocalidad,Dni,Cuil,Nombre,Apellido,Sexo,Nacionalidad,FechaNacimiento,Direccion,Localidad,Provincia,CorreoElectronico,Telefono) 
-values (1,1,14302823,11111111111,"Simon","Molina","Masculino",1,"2021/06/23","Av.Brasil 1233","Boulogne Sur Mer","Buenos Aires","prueba@gmail.com",47103847);
 
-Insert into clientes (idNacionalidad,IdLocalidad,Dni,Cuil,Nombre,Apellido,Sexo,Nacionalidad,FechaNacimiento,Direccion,Localidad,Provincia,CorreoElectronico) 
-values (1,1,24302823,22222222222,"Ramon","Molina","Masculino","Argentino","2021/06/23","Av.Brasil 1233","Boulogne Sur Mer","Buenos Aires","prueba@gmail.com"); #El telefono es opcional 
 
-# Datos harcodeados
-# Creo que la relacion localidad - provincia esta al reves
-insert into nacionalidad (descripcion) values ("Argentina"); #Como el campo es ai entonces tendra como id = 1;
-insert into localidad (descripcion) values ("San isidro");
-insert into provincia (descripcion) values ("Buenos Aires");
+
+
+
 
