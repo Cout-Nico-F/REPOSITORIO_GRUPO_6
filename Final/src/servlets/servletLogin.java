10322930c4a5b165abc3a2fc60c7f2e42b2979e6
Alonso <61801479+AlonsoHS21@users.noreply.github.com>
2021-06-25 17:08:07 -0300

    Fix error import java servlet

--- Final/src/servlets/servletLogin.java
+++ Final/src/servlets/servletLogin.java
@@ -1,13 +1,18 @@
 package servlets;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+// Error the import javax.servlet cannot be resolved
+// solucion: https://stackoverflow.com/questions/4119448/the-import-javax-servlet-cant-be-resolved
+// Donde econtrar servet api.jar http://es.uwenku.com/question/p-dfvukhbm-o.html
 import dao.UsuarioDao;
+import daoImpl.UsuarioDaoImpl;
 import entidad.Usuario;
 
 /**
@@ -45,17 +50,17 @@ public class servletLogin extends HttpServlet {
 			u.setNombreUsuario(request.getParameter("txtNombreUsuario"));
 			u.setContrasenia(request.getParameter("txtContrasenia"));
 			
-			UsuarioDao udao = new UsuarioDao();
-			usuarioEncontrado = getUsuario(u); //Envio el nombre y contrasenia y hago un where en la bd si coinciden lo traigo
+			UsuarioDaoImpl udao = new UsuarioDaoImpl();
+			usuarioEncontrado = udao.getUsuario(u); //Envio el nombre y contrasenia y hago un where en la bd si coinciden lo traigo
 			//Traer usuario devuelve un boolean
 		}
 		if(usuarioEncontrado) { //Si es true
 			session.setAtrribute("mensaje","Usuario correcto");
-			request.setAtrribute("tipoMensaje","succes");
+			request.setAttribute("tipoMensaje","succes");
 		}
 		else {
 			session.setAtrribute("mensaje","Usuario no encontrado");
-			request.setAtrribute("tipoMensaje","danger"); 
+			request.setAttribute("tipoMensaje","danger"); 
 		}
 		
 		RequestDispatcher rd = request.getRequestDispatcher("/Login.jsp");
