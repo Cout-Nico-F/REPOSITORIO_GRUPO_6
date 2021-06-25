7be621f5306992606fc122781b59aec0f9cd22bc
Alonso <61801479+AlonsoHS21@users.noreply.github.com>
2021-06-25 17:08:49 -0300

    Arreglando errores de la clase Usuario y UsuarioDaoImpl

--- Final/src/dao/UsuarioDao.java
+++ Final/src/dao/UsuarioDao.java
@@ -1,5 +1,7 @@
 package dao;
 
+import entidad.Usuario;
+
 public interface UsuarioDao {
 	
 	public boolean getUsuario(Usuario u);
--- Final/src/daoImpl/UsuarioDaoImpl.java
+++ Final/src/daoImpl/UsuarioDaoImpl.java
@@ -1,17 +1,16 @@
 package daoImpl;
 
-import java.sql.ResultSet;
 import java.sql.SQLException;
-
-import entidad.Persona;
+import entidad.Usuario;
 
 public class UsuarioDaoImpl {
 	
 	private static final String getUsuario = "Select * From usuarios Where nombreUsuario='u.getnombreUsuario' and contrasenia='u.getContrasenia'";
 	
-	private boolean getUsuario(Usuario u) throws SQLException
+	public boolean getUsuario(Usuario u) throws SQLException
 	{
 		//Codigo para buscar el usuario y que devuelve un bool en caso de existir
 		//Cuando la bd este terminada haria un procedimiento almacenado
+		return true;
 	}
 }
