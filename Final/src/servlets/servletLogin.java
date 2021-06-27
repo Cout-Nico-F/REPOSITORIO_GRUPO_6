65069cb07636b5ec766cebbb51cd3d3c6dba9744
Alonso <61801479+AlonsoHS21@users.noreply.github.com>
2021-06-26 21:29:12 -0300

    eliminando comentarios de los archivos servlet login y clientes

--- Final/src/servlets/servletLogin.java
+++ Final/src/servlets/servletLogin.java
@@ -15,32 +15,19 @@ import entidad.Usuario;
 import negocio.UsuarioNegocio;
 import negocioImpl.UsuarioNegocioImpl;
 
-
-/**
- * Servlet implementation class servletLogin
- */
 @WebServlet("/servletLogin")
 public class servletLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
     public servletLogin() {
         // TODO Auto-generated constructor stub
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		HttpSession sessionMensaje = request.getSession();
