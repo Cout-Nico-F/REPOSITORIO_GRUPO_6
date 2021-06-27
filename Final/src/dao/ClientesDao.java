b372ef337c2675c10d5997596f35296c78a2cb25
Alonso <61801479+AlonsoHS21@users.noreply.github.com>
2021-06-26 22:03:37 -0300

    Creando ClientesDao y su intefaz

---
+++ Final/src/dao/ClientesDao.java
@@ -0,0 +1,8 @@
+package dao;
+import entidad.Cliente;
+
+public interface ClientesDao {
+	
+	public Cliente insertCliente(Cliente c);
+	
+}
---
+++ Final/src/daoImpl/ClientesDaoImpl.java
@@ -0,0 +1,7 @@
+package daoImpl;
+
+public class ClientesDaoImpl {
+	
+	private static final String insertCliente = "insert into clientes (Dni,IdUsuario,IdNacionalidad,IdLocalidad,Cuil,Nombre,Apellido,Sexo,FechaNacimiento,Direccion,CorreoElectronico) values ('?','?','?','?','?','?','?','?','?','?','?');";
+	
+}
