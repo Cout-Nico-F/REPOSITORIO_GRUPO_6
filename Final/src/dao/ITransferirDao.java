888506252006fb327865d1d1ae428c096025e2d5
Cout-Nico-F <n.ferreiraa@live.com.ar>
2021-07-06 17:07:15 -0300

    Metodo comprobarExistencia

---
+++ Final/src/dao/ITransferirDao.java
@@ -0,0 +1,8 @@
+package dao;
+
+import java.sql.SQLException;
+
+public interface ITransferirDao {
+	public boolean ComprobarExistencia(String cbu) throws SQLException;
+	public boolean ComprobarSaldo(String cbu, float cantidad);
+}
