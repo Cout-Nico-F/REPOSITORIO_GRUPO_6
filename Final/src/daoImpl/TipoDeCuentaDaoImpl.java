8c00630ae63b86d33477187d02b7eb47b4855a13
Alonso <61801479+AlonsoHS21@users.noreply.github.com>
2021-07-06 17:26:52 -0300

    Metodo para traer los tipos de cuenta de un usuario

--- Final/src/daoImpl/TipoDeCuentaDaoImpl.java
+++ Final/src/daoImpl/TipoDeCuentaDaoImpl.java
@@ -10,6 +10,11 @@ public class TipoDeCuentaDaoImpl implements ITipoDeCuentaDao{
 
 	private static final String listaTodos = "select * from tiposdecuenta";
 	private static final String buscar = "select * from tiposdecuenta where idtipocuenta=";
+	// Trae todo lo relacionado de tipo cuentas que correspondan a un usuario
+	private static final String buscarCuentasUsuarios = "Select * from tiposdecuenta inner join cuentas on tiposdecuenta.IdTipoCuenta = cuentas.IdTipoCuenta \r\n" + 
+			"inner join clientes on clientes.dni = cuentas.dni \r\n" + 
+			"inner join usuarios on usuarios.IdUsuario = clientes.IdUsuario\r\n" + 
+			"Where usuarios.IdUsuario = ?";
 	
 	@Override
 	public ArrayList<TipoDeCuenta> listarTiposCuentas() {
@@ -52,5 +57,28 @@ public class TipoDeCuentaDaoImpl implements ITipoDeCuentaDao{
 			return aux;
 		}
 		return aux;
+	}
+
+	@Override
+	public ArrayList<TipoDeCuenta> buscarTiposDeCuentasUsuario(int IDUsuario) {
+		ResultSet rs;
+		PreparedStatement ps;
+		ArrayList<TipoDeCuenta> ListaTipos = new ArrayList<TipoDeCuenta>();
+		Connection conexion = Conexion.getConexion().getSQLConexion();
+		try {
+			ps = conexion.prepareStatement(buscarCuentasUsuarios);
+			ps.setInt(1,IDUsuario);
+			rs = ps.executeQuery();
+			while(rs.next()) {
+				TipoDeCuenta tipoDeCuenta = new TipoDeCuenta();
+				tipoDeCuenta.setDescripcion(rs.getString("descripcion"));
+				tipoDeCuenta.setIdTipoCuenta(rs.getShort("idTipoCuenta"));
+				ListaTipos.add(tipoDeCuenta);
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+			return ListaTipos;
+		}
+		return ListaTipos;
 	}
 }
