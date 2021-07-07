0e8fd4927add5ad1371129c5ee71829ec8cc4936
Cristian Paolini <cristianpaolini3@gmail.com>
2021-07-07 00:34:56 -0300

    Implementación de método validarUsuarioAdmin en ServletAutorizarPrestamos y ServletVistaAdmin.

--- Final/src/servlets/ServletAutorizarPrestamos.java
+++ Final/src/servlets/ServletAutorizarPrestamos.java
@@ -1,6 +1,8 @@
 package servlets;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,9 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import negocio.IAdminNegocio;
 import negocioImpl.AdminNegocioImpl;
 
-/**
- * Servlet implementation class ServletAutorizarPrestamos
- */
+
 @WebServlet("/ServletAutorizarPrestamos")
 public class ServletAutorizarPrestamos extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -25,11 +25,45 @@ public class ServletAutorizarPrestamos extends HttpServlet {
 
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		if (admNeg.validarUsuarioAdmin(request)) {
+			//Acá adentro el código si está logueado
+			
+			
+			
+			
+			
+			
+			//Acá adentro el código si está logueado
+		}else {
+			response.sendRedirect("Login.jsp");
+			return;
+		}
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/AutorizarPrestamos.jsp");
+		rd.forward(request, response);
 
 	}
 
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		if (admNeg.validarUsuarioAdmin(request)) {
+			//Acá adentro el código si está logueado
+			
+			
+			
+			
+			
+			
+			//Acá adentro el código si está logueado
+		}else {
+			response.sendRedirect("Login.jsp");
+			return;
+		}
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/AutorizarPrestamos.jsp");
+		rd.forward(request, response);
 
 	}
 
