d1281cb3e3b0df3ea667081a496a10c60174ae75
Alonso <61801479+AlonsoHS21@users.noreply.github.com>
2021-07-12 17:03:57 -0300

    Insserts de clientes usuarios y cuentas
    Todos minimamente tienen una cuenta pero nose si necesitan agregarle mas de una cuenta a un cliente para probar los abml

--- Final/ScriptingBancos/bdlabo.sql
+++ Final/ScriptingBancos/bdlabo.sql
@@ -102,8 +102,6 @@ alter table clientes add foreign key (IdNacionalidad) references nacionalidades 
 # -- Localidad --
 alter table localidades add foreign key (IdProvincia) references provincias (IdProvincia);
 
-# -- Provincia --
-
 # -- Cuenta --
 ALTER TABLE cuentas AUTO_INCREMENT = 1000000;
 alter table cuentas add foreign key (Dni) references clientes (Dni);
@@ -122,6 +120,27 @@ alter table movimientos add foreign key (CuentaDestino) references Cuentas (Nume
 alter table movimientos add foreign key (IdTipoMovimiento) references tiposMovimientos (IdTipoMovimiento);
 alter table movimientos add constraint check (not(isnull(CuentaOrigen) and isnull(CuentaDestino)));
 
+
+# -------------------------------------------------------------- INSERTS --------------------------------------------------------------
+
+# -- Inserts para Usuarios Admins --
+insert into usuarios (NombreUsuario,Contrasenia,EsAdmin) values ("AlonsoH","1",true); #tendriamos que encriptar la contrasenia el primer usuario va a tener IdUsuario 1 se supone
+insert into usuarios (NombreUsuario,Contrasenia,EsAdmin) values ("NicoF","12",true);
+insert into usuarios (NombreUsuario,Contrasenia,EsAdmin) values ("AndresC","123",true);
+insert into usuarios (NombreUsuario,Contrasenia,EsAdmin) values ("DeniseR","1234",true);
+insert into usuarios (NombreUsuario,Contrasenia,EsAdmin) values ("CristianP","12345",true);
+insert into usuarios (NombreUsuario,Contrasenia,EsAdmin) values ("MarcosL","123456",true);
+# -- Inserts para usuarios clientes --
+insert into usuarios (NombreUsuario,Contrasenia,EsAdmin) values ("Martin","1",false); 
+insert into usuarios (NombreUsuario,Contrasenia,EsAdmin) values ("Lorena","2",false); 
+insert into usuarios (NombreUsuario,Contrasenia,EsAdmin) values ("Gaston","3",false); 
+insert into usuarios (NombreUsuario,Contrasenia,EsAdmin) values ("Fabricio","4",false);
+insert into usuarios (NombreUsuario,Contrasenia,EsAdmin) values ("Simon","5",false); 
+insert into usuarios (NombreUsuario,Contrasenia,EsAdmin) values ("Jose","6",false); 
+insert into usuarios (NombreUsuario,Contrasenia,EsAdmin) values ("Ramon","7",false); 
+insert into usuarios (NombreUsuario,Contrasenia,EsAdmin) values ("Victor","8",false); 
+insert into usuarios (NombreUsuario,Contrasenia,EsAdmin) values ("Diego","9",false);
+
 # -- Paises --
 insert into nacionalidades (Nombre) values ("Argentina");
 insert into nacionalidades (Nombre) values ("Brasil");
@@ -139,8 +158,8 @@ insert into nacionalidades (Nombre) values ("Francia");
 insert into nacionalidades (Nombre) values ("Filipinas");
 insert into nacionalidades (Nombre) values ("Canada");
 
-# -- Paises -- Provincias
-#arg
+# -- Provincias -- 
+# Argentina
 insert into provincias (Nombre) values ("Buenos Aires");
 insert into provincias (Nombre) values ("Cordoba");
 insert into provincias (Nombre) values ("Santa fe");
@@ -151,18 +170,18 @@ insert into provincias (Nombre) values ("Tierra del fuego");
 insert into provincias (Nombre) values ("La pampa");
 insert into provincias (Nombre) values ("Rio negro");
 insert into provincias (Nombre) values ("Entre rios");
-# brasil
+# Brasil
 insert into provincias (Nombre) values ("Bahia");
 insert into provincias (Nombre) values ("Minas gerais");
 insert into provincias (Nombre) values ("Ceara");
-#chile 
+# Chile 
 insert into provincias (Nombre) values ("Santiago");
 insert into provincias (Nombre) values ("Cuyo");
 
+# -- Localidades -- 
 insert into localidades (IdProvincia,Nombre) values (1,"Escobar");
 insert into localidades (IdProvincia,Nombre) values (1,"Pacheco");
 insert into localidades (IdProvincia,Nombre) values (1,"Caba");
-
 insert into localidades (IdProvincia,Nombre) values (2,"EscobarEnBrasil");
 insert into localidades (IdProvincia,Nombre) values (2,"LocalidadBrasil2");
 insert into localidades (IdProvincia,Nombre) values (3,"LocalidadChile1");
@@ -176,25 +195,60 @@ insert into localidades (IdProvincia,Nombre) values (10,"Localidad1");
 insert into localidades (IdProvincia,Nombre) values (11,"Localidad1");
 insert into localidades (IdProvincia,Nombre) values (12,"Localidad1");
 
-
-insert into usuarios (NombreUsuario,Contrasenia,EsAdmin) values ("AlonsoHS20","12345",true);
-
-insert into usuarios (NombreUsuario,Contrasenia,EsAdmin) values ("Nose","123",false);
-insert into usuarios (NombreUsuario,Contrasenia,EsAdmin) values ("Yase","123",false); 
-
+# -- Inserts para Clientes --  
 insert into clientes (Dni,IdUsuario,IdNacionalidad,IdLocalidad,Cuil,Nombre,Apellido,Sexo,FechaNacimiento,Direccion,CorreoElectronico,TelefonoFijo,Celular) 
-values (14203944,2,1,1,111111111111,"Nose","Valdez","Masculino","2021/06/25","Av.Siempre viva 123","prueba@gmail.com",01123948373, 1523344556);
-
+values (11111111,1,1,1,121111111111,"Alonso","Huarcaya","Masculino","2021/06/25","Av.Siempre viva 123","prueba1@gmail.com",01123948373, 1523344556);
 insert into clientes (Dni,IdUsuario,IdNacionalidad,IdLocalidad,Cuil,Nombre,Apellido,Sexo,FechaNacimiento,Direccion,CorreoElectronico,TelefonoFijo,Celular) 
-values (0303456,3,1,1,111111222111,"Yase","Gomez","Masculino","2021/06/25","Av.Nuenca muerta 987","prueba@gmail.com",01123848373, 1523344557);
+values (22222222,2,1,1,131111222111,"Nicolas","Ferreira","Masculino","2021/06/25","Av.Nuenca muerta 987","prueba2@gmail.com",01123848373, 1523344557);
+insert into clientes (Dni,IdUsuario,IdNacionalidad,IdLocalidad,Cuil,Nombre,Apellido,Sexo,FechaNacimiento,Direccion,CorreoElectronico,TelefonoFijo,Celular) 
+values (33333333,3,1,1,141111222111,"Anders","Cacchione","Masculino","2021/06/25","Av.Nuenca muerta 987","prueba3@gmail.com",01123848373, 1523344557);
+insert into clientes (Dni,IdUsuario,IdNacionalidad,IdLocalidad,Cuil,Nombre,Apellido,Sexo,FechaNacimiento,Direccion,CorreoElectronico,TelefonoFijo,Celular) 
+values (44444444,4,1,1,151111222111,"Denise","Roccia","Femenino","2021/06/25","Av.Nuenca muerta 987","prueba4@gmail.com",01123848373, 1523344557);
+insert into clientes (Dni,IdUsuario,IdNacionalidad,IdLocalidad,Cuil,Nombre,Apellido,Sexo,FechaNacimiento,Direccion,CorreoElectronico,TelefonoFijo,Celular) 
+values (55555555,5,1,1,161111222111,"Cristian","Paolini","Masculino","2021/06/25","Av.Nuenca muerta 987","prueba5@gmail.com",01123848373, 1523344557);
+insert into clientes (Dni,IdUsuario,IdNacionalidad,IdLocalidad,Cuil,Nombre,Apellido,Sexo,FechaNacimiento,Direccion,CorreoElectronico,TelefonoFijo,Celular) 
+values (66666666,6,1,1,171111222111,"Marcos","Ledesma","Masculino","2021/06/25","Av.Nuenca muerta 987","prueba6@gmail.com",01123848373, 1523344557);
+insert into clientes (Dni,IdUsuario,IdNacionalidad,IdLocalidad,Cuil,Nombre,Apellido,Sexo,FechaNacimiento,Direccion,CorreoElectronico,TelefonoFijo,Celular) 
+values (77777777,7,1,1,181111222111,"Martin","Gomez","Masculino","2021/06/25","Av.Nuenca muerta 987","prueba7@gmail.com",01123848373, 1523344557);
+insert into clientes (Dni,IdUsuario,IdNacionalidad,IdLocalidad,Cuil,Nombre,Apellido,Sexo,FechaNacimiento,Direccion,CorreoElectronico,TelefonoFijo,Celular) 
+values (88888888,8,1,1,192111222111,"Lorena","Gomez","Masculino","2021/06/25","Av.Nuenca muerta 987","prueba8@gmail.com",01123848373, 1523344557);
+insert into clientes (Dni,IdUsuario,IdNacionalidad,IdLocalidad,Cuil,Nombre,Apellido,Sexo,FechaNacimiento,Direccion,CorreoElectronico,TelefonoFijo,Celular) 
+values (99999999,9,1,1,101111222111,"Gaston","Dalmau","Masculino","2021/06/25","Av.Nuenca muerta 987","prueba9@gmail.com",01123848373, 1523344557);
+insert into clientes (Dni,IdUsuario,IdNacionalidad,IdLocalidad,Cuil,Nombre,Apellido,Sexo,FechaNacimiento,Direccion,CorreoElectronico,TelefonoFijo,Celular) 
+values (10101010,10,1,1,191111222111,"Fabricio","Grande","Masculino","2021/06/25","Av.Nuenca muerta 987","prueba10@gmail.com",01123848373, 1523344557);
+insert into clientes (Dni,IdUsuario,IdNacionalidad,IdLocalidad,Cuil,Nombre,Apellido,Sexo,FechaNacimiento,Direccion,CorreoElectronico,TelefonoFijo,Celular) 
+values (11111112,11,1,1,201111222111,"Simon","Bolivar","Masculino","2021/06/25","Av.Nuenca muerta 987","prueba11@gmail.com",01123848373, 1523344557);
+insert into clientes (Dni,IdUsuario,IdNacionalidad,IdLocalidad,Cuil,Nombre,Apellido,Sexo,FechaNacimiento,Direccion,CorreoElectronico,TelefonoFijo,Celular) 
+values (12121212,12,1,1,251111222111,"Jose","Jose","Masculino","2021/06/25","Av.Nuenca muerta 987","prueba12@gmail.com",01123848373, 1523344557);
+insert into clientes (Dni,IdUsuario,IdNacionalidad,IdLocalidad,Cuil,Nombre,Apellido,Sexo,FechaNacimiento,Direccion,CorreoElectronico,TelefonoFijo,Celular) 
+values (13131313,13,1,1,211111222111,"Ramon","Valdez","Masculino","2021/06/25","Av.Nuenca muerta 987","prueba13@gmail.com",01123848373, 1523344557);
+insert into clientes (Dni,IdUsuario,IdNacionalidad,IdLocalidad,Cuil,Nombre,Apellido,Sexo,FechaNacimiento,Direccion,CorreoElectronico,TelefonoFijo,Celular) 
+values (14141414,14,1,1,221111222111,"Victor","Domingo","Masculino","2021/06/25","Av.Nuenca muerta 987","prueba14@gmail.com",01123848373, 1523344557);
+insert into clientes (Dni,IdUsuario,IdNacionalidad,IdLocalidad,Cuil,Nombre,Apellido,Sexo,FechaNacimiento,Direccion,CorreoElectronico,TelefonoFijo,Celular) 
+values (15151515,15,1,1,231111222111,"Diego","Maradona","Masculino","2021/06/25","Av.Nuenca muerta 987","prueba15@gmail.com",01123848373, 1523344557);
 
+# -- Inserts tipos de cuenta -- 
 insert into tiposdecuenta (Descripcion) values ("Caja de Ahorro");
 insert into tiposdecuenta (Descripcion) values ("Cuenta Corriente");
 
-insert into cuentas (numerocuenta,dni,idtipocuenta,saldo,cbu,fechacreacion) values (123813724,14203944,1,10000,124124123,current_date());
-insert into cuentas (numerocuenta,dni,idtipocuenta,saldo,cbu,fechacreacion) values (123813725,14203944,2,10000,124124124,current_date());
-insert into cuentas (numerocuenta,dni,idtipocuenta,saldo,cbu,fechacreacion) values (123813726,null,1,0,124124125,current_date());
+# -- Inserts cuentas -- 
+insert into cuentas (numerocuenta,dni,idtipocuenta,saldo,cbu,fechacreacion) values (123813724,11111111,1,10000,121111111111,current_date());
+insert into cuentas (numerocuenta,dni,idtipocuenta,saldo,cbu,fechacreacion) values (123813725,22222222,2,10000,131111222111,current_date());
+insert into cuentas (numerocuenta,dni,idtipocuenta,saldo,cbu,fechacreacion) values (123813726,33333333,1,10000,141111222111,current_date());
+insert into cuentas (numerocuenta,dni,idtipocuenta,saldo,cbu,fechacreacion) values (123813727,44444444,2,10000,151111222111,current_date());
+insert into cuentas (numerocuenta,dni,idtipocuenta,saldo,cbu,fechacreacion) values (123813728,55555555,2,10000,161111222111,current_date());
+insert into cuentas (numerocuenta,dni,idtipocuenta,saldo,cbu,fechacreacion) values (123813729,66666666,1,10000,171111222111,current_date());
+insert into cuentas (numerocuenta,dni,idtipocuenta,saldo,cbu,fechacreacion) values (123813710,77777777,2,10000,181111222111,current_date());
+insert into cuentas (numerocuenta,dni,idtipocuenta,saldo,cbu,fechacreacion) values (123813711,88888888,2,10000,192111222111,current_date());
+insert into cuentas (numerocuenta,dni,idtipocuenta,saldo,cbu,fechacreacion) values (123813712,99999999,2,10000,101111222111,current_date());
+insert into cuentas (numerocuenta,dni,idtipocuenta,saldo,cbu,fechacreacion) values (123813713,10101010,2,10000,191111222111,current_date());
+insert into cuentas (numerocuenta,dni,idtipocuenta,saldo,cbu,fechacreacion) values (123813714,11111112,1,10000,201111222111,current_date());
+insert into cuentas (numerocuenta,dni,idtipocuenta,saldo,cbu,fechacreacion) values (123813715,12121212,2,10000,251111222111,current_date());
+insert into cuentas (numerocuenta,dni,idtipocuenta,saldo,cbu,fechacreacion) values (123813716,13131313,1,10000,211111222111,current_date());
+insert into cuentas (numerocuenta,dni,idtipocuenta,saldo,cbu,fechacreacion) values (123813717,14141414,2,10000,221111222111,current_date());
+insert into cuentas (numerocuenta,dni,idtipocuenta,saldo,cbu,fechacreacion) values (123813718,15151515,1,10000,231111222111,current_date());
 
+# -- Inserts tipos de movimientos -- 
 insert into tiposmovimientos (descripcion) values ("Alta de cuenta");
 insert into tiposmovimientos (descripcion) values ("Alta de prestamo");
 insert into tiposmovimientos (descripcion) values ("Pago de prestamo");
@@ -202,10 +256,12 @@ insert into tiposmovimientos (descripcion) values ("Transferencia");
 
 insert into movimientos (idtipomovimiento,cuentaorigen,cuentadestino,fecha,detalles,importe) values (2,123813724,123813726,current_timestamp(),"Alta de cuenta", 10000);
 
-insert into prestamos (numerocuenta,dni,fecha,importesolicitado,importeapagar,montomensual,cuotas,estado) values (123813724,14203944,current_date(),50000,65000, 6500, 10, 3);
-insert into prestamos (numerocuenta,dni,fecha,importesolicitado,importeapagar,montomensual,cuotas,estado) values (123813724,14203944,current_date(),5000,6500, 650, 10, 3);
+# -- Estan los dni de los primeros clientes 
+insert into prestamos (numerocuenta,dni,fecha,importesolicitado,importeapagar,montomensual,cuotas,estado) values (123813724,11111111,current_date(),50000,65000, 6500, 10, 3);
+insert into prestamos (numerocuenta,dni,fecha,importesolicitado,importeapagar,montomensual,cuotas,estado) values (123813724,22222222,current_date(),5000,6500, 650, 10, 3);
 
-insert into prestamos (numerocuenta,dni,fecha,importesolicitado,importeapagar,montomensual,cuotas,estado) values (123813724,14203944,current_date(),1000,1200, 120, 10, 1);
+# -- Estan los dni de los primeros clientes 
+insert into prestamos (numerocuenta,dni,fecha,importesolicitado,importeapagar,montomensual,cuotas,estado) values (123813724,11111111,current_date(),1000,1200, 120, 10, 1);
 
 insert into cuotas (IdPrestamos,numerocuota,importe,FechaVencimiento,FechaPago) values (1,1,6500,DATE_FORMAT("2021-08-07", "%Y" "%m" "%d"),null);
 insert into cuotas (IdPrestamos,numerocuota,importe,FechaVencimiento,FechaPago) values (1,2,6500,DATE_FORMAT("2021-09-07", "%Y" "%m" "%d"),null);
